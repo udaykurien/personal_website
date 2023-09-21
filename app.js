410d19ad3f7aadd356ecf75b6ea27c9a34ebd7b7
nuclear <nuclear@fedora.linx>
2023-09-21 18:22:30 -0400

    Initial node.js working commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');app.get("/", function(req, res){
+    res.render("home");
+});
+app.get("/bye", function(req, res){
+  res.render("bye");
+});
+
+app.listen(3000, function(){
+     console.log("server is running on port" + 3000);});
