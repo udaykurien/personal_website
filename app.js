ef97db83970111389ce8a13afceb7fa019b8c83d
nuclear <nuclear@fedora.linx>
2023-09-21 20:40:15 -0400

    Added basic resume page

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 app.set('view engine', 'ejs');
 
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-app.get("/", function(req, res){
+app.get("/home", function(req, res){
     res.render("home");
 });
 app.get("/bye", function(req, res){
   res.render("bye");
+});
+app.get("/resume", function(req, res){
+  res.render("resume");
 });
 
 app.listen(3000, function(){
