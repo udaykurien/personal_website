8623a5b5ae0b05c2b678e980c978c42f16ba92e2
nuclear <nuclear@fedora.linx>
2023-09-24 16:42:49 -0400

    split app.js into app.js and router.js

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var app = express();
+// Set up EJS as the view engine
 app.set('view engine', 'ejs');
 
-var express = require('express');
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname + '/public')));
+// Include your routes
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
-app.get("/home", function(req, res){
-    res.render("home");
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-app.get("/bye", function(req, res){
-  res.render("bye");
-});
-app.get("/resume", function(req, res){
-  res.render("resume");
-});
-app.get("/publications", function(req, res){
-  res.render("publications");
-});
-app.get("/skills", function(req, res){
-  res.render("skills");
-});
-app.get("/media-coverage", function(req, res){
-  res.render("media-coverage");
-});
-
-app.listen(3000, function(){
-     console.log("server is running on port" + 3000);});
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+  });
+
+router.get('/home', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+router.get("/bye", function(req, res){
+    res.render("bye");
+});
+
+router.get("/resume", function(req, res){
+    res.render("resume");
+});
+
+router.get("/publications", function(req, res){
+    res.render("publications");
+});
+
+router.get("/skills", function(req, res){
+    res.render("skills");
+});
+
+router.get("/media-coverage", function(req, res){
+    res.render("media-coverage");
+});
+
+module.exports = router;  
